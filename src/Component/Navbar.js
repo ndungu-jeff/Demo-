0c9e3177eaddbaf41d4ba7d0adfc21c6907d6806
jeff ndungu <ndungujeff.ke@gmail.com>
2023-01-23 09:24:51 +0300

    Done

---
+++ src/Component/Navbar.js
@@ -0,0 +1,23 @@
+import { actors } from "../data";
+
+function Actors() {
+  
+  const getActors = actors.map((actor) => (
+    <div key={actor.name}>
+      <h2>{actor.name}</h2>
+      <ul>
+        {actor.movies.map((movie) => (
+          <li key={movie}>{movie}</li>
+        ))}
+      </ul>
+    </div>
+  ));
+
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      {getActors}
+    </div>
+  );
+}
+export default Actors;
